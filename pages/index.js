d727f2095c0745db761a2b83e2fabe1ed63faf3a
citizenlou <louis.sutton.iii@gmail.com>
2021-07-09 15:04:22 -0500

    Fixed input field container

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,14 @@ export default function Home() {
         <h1 className="title-soon">
           We are launching <span className="span-soon">soon!</span>
         </h1>
-        <p className="w-auto font-thin mb-8">Subscribe and get notified</p>
+        <p className="w-auto font-normal text-opacity-25 text-gray mb-8">Subscribe and get notified</p>
         <div className="input-container">
-          <input
-            className="border-2 border-gray-200 mb-4 h-12 w-auto rounded-full py-3 px-6 shadow-inner"
-            type="text"
-          ></input>
+          <div className="md:mr-6">
+            <input
+              className="border-2 border-gray-200 mb-4 h-12 rounded-full py-3 px-6 shadow-inner"
+              type="text"
+            ></input>
+          </div>
           {/* button below */}
           <div style={{
             margin: "auto",
@@ -35,7 +37,7 @@ export default function Home() {
         </div>
         <img
           src="/static/images/dashboard.png"
-          width="70%"
+          width="80%"
           height="auto"
           alt=""
         />
